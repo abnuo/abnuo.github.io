10035db4ef79edcd419c1ba4064c787a1a614453
abnuo <66645994+abnuo@users.noreply.github.com>
2022-10-28 21:30:02 +0100

    Create munch.js

---
+++ scripts/munch.js
@@ -0,0 +1,14 @@
+// <![CDATA[
+for (var x = 0; x < 256; x++) {
+  for (var y = 0; y < 256; y++) {
+    var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+    var r = x^y;
+    rect.setAttribute("x", x);
+    rect.setAttribute("y", y);
+    rect.setAttribute("height", "1");
+    rect.setAttribute("width", "1");
+    rect.setAttribute("fill", "hsl("+r%361+",100%,50%)");
+    document.getElementById('svg').appendChild(rect);
+  }
+}
+// ]]>
